2b7c66558780bb8c65f8fab93de3bbe310b28b05
Cecile Muller <git@wildpeaks.fr>
2016-10-20 02:53:30 +0200

    Application renders a cube
    Converted the example from
    https://threejs.org/docs/index.html#Manual/Introduction/Creating_a_scene

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # example-threejs-esmodule
 
-Example application that uses Rollup and the ES Module bundle of [three.js]().
+Example application that uses Rollup and the ES Module bundle of [three.js](https://threejs.org/).
 
 Until the [official three module](https://www.npmjs.com/package/three) has a `jsnext:main` (or `module`) entry,
 this example uses the forked [wildpeaks/three.js](https://github.com/wildpeaks/three.js) repository to find the ES bundle.
 
-The application references the bundle using the `import` keyword:
+The application references the bundle using the `import` keyword, for example:
 
 	import {Vector3} from 'three';
 
+[Demo](http://wildpeaks.github.io/example-threejs-esmodule/)
--- rollup.config.js
+++ rollup.config.js
@@ -16,22 +16,28 @@ export default {
 		})
 	],
 	targets: [
+
+		// Web application (self-executing function)
 		{
 			format: 'iife',
 			sourceMap: false,
 			moduleName: 'application',
-			dest: 'dist/bundle.iife.js'
-		},
-		{
-			format: 'umd',
-			sourceMap: false,
-			moduleName: 'application',
-			dest: 'dist/bundle.umd.js'
-		},
-		{
-			format: 'es',
-			sourceMap: false,
-			dest: 'dist/bundle.es6.js'
+			dest: 'docs/bundle.iife.js'
 		}
+
+		// Library (Universal Module format)
+		// {
+		// 	format: 'umd',
+		// 	sourceMap: false,
+		// 	moduleName: 'application',
+		// 	dest: 'docs/bundle.umd.js'
+		// },
+
+		// Library (ES Module format)
+		// {
+		// 	format: 'es',
+		// 	sourceMap: false,
+		// 	dest: 'docs/bundle.es6.js'
+		// }
 	]
 };
